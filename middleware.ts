fdc32746c034102a8399522b1582ddefc149f1a7
Nikita Vassiljev <vasnik@tlu.ee>
2025-11-19 22:42:22 +0200

    (fix): removed supabase middleware that interfered with api

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { updateSession } from "@/lib/supabase/middleware";
 import { type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+ /// return await updateSession(request);
 }
 
 export const config = {
